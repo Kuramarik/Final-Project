1f772ce26164eaa694ebfe23f0d1ab224a7c520e
adtpstudent <atdpstudent@no-email.ocf.berkeley.edu>
2025-07-15 14:38:06 -0700

    WORKING: Character base stat allocation

--- vue-app/src/components/Character.vue
+++ vue-app/src/components/Character.vue
@@ -1,6 +1,32 @@
 <script>
-    /*data(){baseStats object,
-    level variable }
+export default{
+    data() {
+        return {
+            level:10,
+            allocLevel:10,
+            baseStats:{
+                attack:1,
+                magic:1,
+                defense:1,
+                hp:10
+            }
+        }
+    },
+    methods: {
+        modifyStats(icon, stat){
+            //console.log(this.baseStats[stat])
+            if(icon=='+'){
+                this.baseStats[stat]++
+                this.allocLevel--
+            }
+            else if(icon=='-'){
+                this.baseStats[stat]--
+                this.allocLevel++
+            }
+        }
+    }
+}
+    /*
     
     methods{
     modifyStats method if + or -
@@ -9,7 +35,20 @@
 
 <template>
     <!-- Stats with + and - buttons -->
-     <p>character goes here</p>
+     <ul>Character Stats - Level {{this.level}}. You have {{ this.allocLevel }} points
+        <li>attack:<br>
+            <button v-if="this.baseStats.attack>1" @click="modifyStats('-', 'attack')">-</button>{{ this.baseStats.attack }}<button v-if="this.allocLevel>0" @click="modifyStats('+', 'attack')">+</button>
+        </li>
+        <li>magic:<br>
+            <button v-if="this.baseStats.magic>1" @click="modifyStats('-', 'magic')">-</button>{{ this.baseStats.magic }}<button v-if="this.allocLevel>0" @click="modifyStats('+', 'magic')">+</button>
+        </li>
+        <li>defense:<br>
+            <button v-if="this.baseStats.defense>1" @click="modifyStats('-', 'defense')">-</button>{{ this.baseStats.defense }}<button v-if="this.allocLevel>0" @click="modifyStats('+', 'defense')">+</button>
+        </li>
+        <li>health:<br>
+            <button v-if="this.baseStats.hp>10" @click="modifyStats('-', 'hp')">-</button>{{ this.baseStats.hp }}<button v-if="this.allocLevel>0" @click="modifyStats('+', 'hp')">+</button>
+        </li>
+     </ul>
 </template>
 
 <style></style>
